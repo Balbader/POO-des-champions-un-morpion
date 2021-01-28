a49c3b2e8d6659a0cb9a94b94f5c9fe92fead777
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-28 14:29:12 +0100

    update

--- lib/app/player.rb
+++ lib/app/player.rb
@@ -18,15 +18,21 @@ class Player
 
   def token
     puts "So... Are you more of an X person or an O type of person???"
+    sleep(0.5)
     puts "Just in case you didn't get the question, choose between X and O..."
+    sleep(0.5)
     puts "Oh.. 1 last thing.. make sure they're caps.."
+    sleep(0.5)
     puts "Go ahead.. choose now:"
     print "> "
     token = gets.chomp.upcase
     while token != "X" && token != "O"
       puts "What an idiot you are !!!"
+      sleep(0.5)
       puts "So apparently.. that question was a bit too complex for your little peanut of a brain..."
+      sleep(0.5)
       puts "Hmmmmm... A coloring book might be more suited for your entertainment needs..."
+      sleep(0.5)
       puts "Anyway... Give it another try.. Choose between X and O you genius.."
       print "> "
       token = gets.chomp.upcase
