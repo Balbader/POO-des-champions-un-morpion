80b2f41dae731587e0c40991ee18b1f4c6e22d7a
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-28 14:12:04 +0100

    update to back to the future

--- lib/view/show.rb
+++ lib/view/show.rb
@@ -1,18 +1,19 @@
-class Show
-end
-  
-  board = [" ", "1", "2", "3", "A", ".", ".", ".", "B", ".", ".", ".", "C", ".", ".", "."]
+require_relative '../app/board'
 
-def show_board(board)
-  puts " " * 5 + "1" + " " * 3 + "2" + " " * 3 + "3"
-  puts "_" * 15
-  puts " #{board[0]}   #{board[1]}   #{board[2]}   #{board[3]} "
-  puts "---------------"
-  puts " #{board[4]} | #{board[5]} | #{board[6]} | #{board[7]} |"
-  puts "---------------"
-  puts " #{board[8]} | #{board[9]} | #{board[10]} | #{board[11]} |"
-  puts "---------------"
-  puts " #{board[12]} | #{board[13]} | #{board[14]} | #{board[15]} |"
+  #class Show
+    #attr_accessor :table
+table = Board.new
+
+
+
+def show_board(table)
+  puts " " * 5 + "1" + " " * 3 + "2" + " " * 3 + "3"                              
+  puts "-" *16
+  puts " A | #{table.arr_boardcase[0].token} | #{table.arr_boardcase[1].token} | #{table.arr_boardcase[2].token} |"
+  puts "-" *16
+  puts " B | #{table.arr_boardcase[3].token} | #{table.arr_boardcase[4].token} | #{table.arr_boardcase[5].token} |"
+  puts "-" *16
+  puts " C | #{table.arr_boardcase[6].token} | #{table.arr_boardcase[7].token} | #{table.arr_boardcase[8].token} |"
 end
 
-show_board(board)
+show_board(table)
